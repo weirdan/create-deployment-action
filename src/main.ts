535c8096b9d9bcd6d8a1d999ab9ee86407bcd71e
Bruce Weirdan <weirdan@gmail.com>
2020-05-06 04:28:56 +0300

    Please eslint

--- src/main.ts
+++ src/main.ts
@@ -1,65 +1,83 @@
 import * as core from '@actions/core'
-import * as github from "@actions/github"
+import * as github from '@actions/github'
 
 const boolOpt = (val: string): boolean => {
-  if (val === "true") {
+  if (val === 'true') {
     return true
   }
-  if (val === "false") {
+  if (val === 'false') {
     return false
   }
   throw new Error(`Unexpected boolean value: ${val}`)
 }
 
-const jsonOpt = (val: string): any => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isArrayOfStrings = (value: any): value is string[] =>
+  Array.isArray(value) && value.every(item => typeof item === 'string')
+
+const jsonList = (val: string): string[] => {
   try {
-    return JSON.parse(val);
+    const ret = JSON.parse(val)
+    if (!isArrayOfStrings(ret)) {
+      throw new Error(`Unexpected JSON value: ${val}`)
+    }
+    return ret
   } catch {
     throw new Error(`Unexpected JSON value: ${val}`)
   }
 }
 
-const refOpt = (val: string): string => val.replace(/^refs\/heads\//,'')
+const refOpt = (val: string): string => val.replace(/^refs\/heads\//, '')
 
 async function run(): Promise<void> {
   try {
     const context = github.context
 
-    const token = core.getInput("token", {required: true})
-    const ref = refOpt(core.getInput("ref", {required: false}) || context.ref)
-    const task = core.getInput("task", {required: false}) || "deploy"
-    const auto_merge = boolOpt(core.getInput("auto_merge", {required: false}) || "true")
-    const payload = core.getInput("payload", {required: false}) || ""
-    const environment = core.getInput("environment", {required: false}) || "production"
-    const description = core.getInput("description", {required: false}) || ""
-    const transient_environment = boolOpt(core.getInput("transient_environment", {required: false}) || "false")
-    const production_environment = boolOpt(core.getInput("production_environment", {required: false}) || "false")
-    const required_contexts = jsonOpt(core.getInput("required_contexts", {required:false}) || "[]")
+    const token = core.getInput('token', {required: true})
+    const ref = refOpt(core.getInput('ref', {required: false}) || context.ref)
+    const task = core.getInput('task', {required: false}) || 'deploy'
+    const autoMerge = boolOpt(
+      core.getInput('auto_merge', {required: false}) || 'true'
+    )
+    const payload = core.getInput('payload', {required: false}) || ''
+    const environment =
+      core.getInput('environment', {required: false}) || 'production'
+    const description = core.getInput('description', {required: false}) || ''
+    const transientEnvironment = boolOpt(
+      core.getInput('transient_environment', {required: false}) || 'false'
+    )
+    const productionEnvironment = boolOpt(
+      core.getInput('production_environment', {required: false}) || 'false'
+    )
+    const requiredContexts = jsonList(
+      core.getInput('required_contexts', {required: false}) || '[]'
+    )
 
+    const client = new github.GitHub(token, {previews: ['flash', 'ant-man']})
 
-    const client = new github.GitHub(token, {previews: ["flash", "ant-man"]})
-
+    // eslint-
     const request = {
       owner: context.repo.owner,
       repo: context.repo.repo,
       ref,
       task,
-      auto_merge,
+      auto_merge: autoMerge, // eslint-disable-line @typescript-eslint/camelcase
       payload,
       environment,
       description,
-      transient_environment,
-      production_environment,
-      required_contexts
+      transient_environment: transientEnvironment, // eslint-disable-line @typescript-eslint/camelcase
+      production_environment: productionEnvironment, // eslint-disable-line @typescript-eslint/camelcase
+      required_contexts: requiredContexts // eslint-disable-line @typescript-eslint/camelcase
     }
 
-    core.info("Creating deployment...")
+    core.info('Creating deployment...')
     core.debug(`Deployment params: ${JSON.stringify(request)}`)
 
     const deployment = await client.repos.createDeployment(request)
-    core.info(`Successfully created deployment: ${deployment.data.id.toString()}`)
-    core.setOutput("deployment_id", deployment.data.id.toString())
-
+    core.info(
+      `Successfully created deployment: ${deployment.data.id.toString()}`
+    )
+    core.setOutput('deployment_id', deployment.data.id.toString())
   } catch (error) {
     core.error(error)
     core.setFailed(error.message)
